6abcb0a44dfd8c673df94004b7a9ce2dc6d21e7b
Nikoloz <nikoloz.mamyan@lnstrade.fr>
2025-09-04 08:33:08 +0200

    login import fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router"
-import Login from "@/views/login.vue"
+import Login from "@/views/Login.vue"
 // import Register from "@/views/Register.vue"
 // import Dashboard from "@/views/Dashboard.vue"
 
